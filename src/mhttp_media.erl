00c7ab36cac0d89503b36ed17b3bfcf05e3b403f
Nicolas Martyanoff <khaelin@gmail.com>
2021-03-09 10:03:38 +0100

    introduce media type parsing

---
+++ src/mhttp_media.erl
@@ -0,0 +1,62 @@
+-module(mhttp_media).
+
+-export([parse_type/1]).
+
+-export_type([parameter_name/0, parameter_value/0, parameters/0, type/0]).
+
+-type error_reason() ::
+        invalid_format
+      | {invalid_parameter, binary()}.
+
+-type parameter_name() :: binary().
+-type parameter_value() :: binary().
+-type parameter() :: {parameter_name(), parameter_value()}.
+-type parameters() :: [parameter()].
+
+-type type() :: {binary(), binary()}
+              | {binary(), binary(), parameters()}.
+
+-spec parse_type(binary()) -> {ok, type()} | {error, error_reason()}.
+parse_type(Data) ->
+  try
+    {ok, parse_type_1(Data)}
+  catch
+    throw:{error, Reason} ->
+      {error, Reason}
+  end.
+
+-spec parse_type_1(binary()) -> type().
+parse_type_1(Data) ->
+  case binary:split(Data, <<"/">>) of
+    [<<>>, _] ->
+      throw({error, invalid_format});
+    [Type, Rest] ->
+      case binary:split(Rest, <<";">>) of
+        [<<>>, _] ->
+          throw({error, invalid_format});
+        [Subtype0, ParametersData] ->
+          Subtype = string:trim(Subtype0, trailing),
+          {Type, Subtype, parse_parameters(ParametersData)};
+        [<<>>] ->
+          throw({error, invalid_format});
+        [Subtype] ->
+          {Type, Subtype}
+      end;
+    [_] ->
+      throw({error, invalid_format})
+  end.
+
+-spec parse_parameters(binary()) -> parameters().
+parse_parameters(Data) ->
+  lists:map(fun parse_parameter/1, binary:split(Data, <<";">>, [global])).
+
+-spec parse_parameter(binary()) -> parameter().
+parse_parameter(Data) ->
+  case binary:split(Data, <<"=">>) of
+    [Name, Value] when Name =:= <<>>; Value =:= <<>> ->
+      throw({error, {invalid_parameter, Data}});
+    [Name, Value] ->
+      {string:trim(Name, both), string:trim(Value, both)};
+    [_] ->
+      throw({error, {invalid_parameter, Data}})
+  end.
---
+++ test/mhttp_media_tests.erl
@@ -0,0 +1,34 @@
+-module(mhttp_media_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+parse_type_test_() ->
+  [?_assertEqual({ok, {<<"text">>, <<"plain">>}},
+                 mhttp_media:parse_type(<<"text/plain">>)),
+   ?_assertEqual({ok, {<<"Text">>, <<"Plain">>}},
+                 mhttp_media:parse_type(<<"Text/Plain">>)),
+   ?_assertEqual({ok, {<<"text">>, <<"plain">>,
+                       [{<<"encoding">>, <<"UTF-8">>}]}},
+                 mhttp_media:parse_type(<<"text/plain;encoding=UTF-8">>)),
+   ?_assertEqual({ok, {<<"x-mhttp">>, <<"test">>,
+                       [{<<"a">>, <<"1">>}, {<<"b">>, <<"2">>}]}},
+                 mhttp_media:parse_type(<<"x-mhttp/test;a=1;b=2">>)),
+   ?_assertEqual({ok, {<<"x-mhttp">>, <<"test">>,
+                       [{<<"a">>, <<"1">>}, {<<"b">>, <<"2">>}]}},
+                 mhttp_media:parse_type(<<"x-mhttp/test ;  a =1\t;\nb=  2">>)),
+   ?_assertEqual({error, invalid_format},
+                 mhttp_media:parse_type(<<"foo">>)),
+   ?_assertEqual({error, invalid_format},
+                 mhttp_media:parse_type(<<"foo/">>)),
+   ?_assertEqual({error, invalid_format},
+                 mhttp_media:parse_type(<<"/bar">>)),
+   ?_assertEqual({error, invalid_format},
+                 mhttp_media:parse_type(<<"/">>)),
+   ?_assertEqual({error, {invalid_parameter, <<"">>}},
+                 mhttp_media:parse_type(<<"foo/bar;">>)),
+   ?_assertEqual({error, {invalid_parameter, <<"a=">>}},
+                 mhttp_media:parse_type(<<"foo/bar;a=">>)),
+   ?_assertEqual({error, {invalid_parameter, <<"=1">>}},
+                 mhttp_media:parse_type(<<"foo/bar;=1">>)),
+   ?_assertEqual({error, {invalid_parameter, <<"=">>}},
+                 mhttp_media:parse_type(<<"foo/bar;=">>))].
